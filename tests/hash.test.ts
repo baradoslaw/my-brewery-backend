fbff8729037981dad7095f60bf803e5288e84b38
RadoslawBaran <baradoslaw@gmail.com>
2023-03-02 17:56:57 +0100

    Add makeHash function (creating hashes with bcrypt) and fix test for it, so it works with asynchronous function now.

--- tests/hash.test.ts
+++ tests/hash.test.ts
@@ -1,6 +1,8 @@
-test('makeHash creates valid bcrypt hash (60 characters).', () => {
+import {makeHash} from "../utils/hash";
+
+test('makeHash creates valid bcrypt hash (60 characters).', async () => {
     const text = 'Example password';
-    const newHash = makeHash(text);
+    const newHash = await makeHash(text);
 
     expect(newHash.length).toBe(60);
 });
